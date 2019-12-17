d3f8e5259983c87c54635223a748f466b3320363
Putta Vijay Kanth <42271835+VijayPutta@users.noreply.github.com>
2019-12-17 07:57:25 +0530

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -113,8 +113,9 @@ class BinaryTree {
         tree.root.left.right = new Node(5); 
         tree.root.right.left = new Node(6); 
         tree.root.right.right = new Node(7); 
-       // tree.inorder(); 
-        //tree.preorder();
+        tree.inorder(); 
+        tree.preorder();
+        tree.postorder();
         tree.postorderonestack();
     }
 }
