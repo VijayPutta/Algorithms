7544baafd193a7698c33e6d3ce3a2ca1508fb538
Putta Vijay Kanth <42271835+VijayPutta@users.noreply.github.com>
2020-01-03 07:22:13 +0530

    Update Reverseonlyletters.java

--- String/Reverseonlyletters.java
+++ String/Reverseonlyletters.java
@@ -1,3 +1,17 @@
+/*Example 1:
+
+Input: "ab-cd"
+Output: "dc-ba"
+Example 2:
+
+Input: "a-bC-dEf-ghIj"
+Output: "j-Ih-gfE-dCba"
+Example 3:
+
+Input: "Test1ng-Leet=code-Q!"
+Output: "Qedo1ct-eeLg=ntse-T!"
+ 
+*/
 class Solution {
     public String reverseOnlyLetters(String S) {
         int i = 0, j = S.length() - 1;
